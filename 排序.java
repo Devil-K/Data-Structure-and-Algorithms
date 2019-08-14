34ae5db0dc0746952fa65d4bdc1ea6b1c42801fd
Devil-K <59968873@qq.com>
2019-08-14 11:32:58 +0800

    test commit

--- 排序.java
+++ 排序.java
@@ -6,11 +6,15 @@ public class 排序 {
 	public static void main(String[] args) {
 		int[] a = { 4, 2, 3, 1, 5, 3 };
 		// 0 1 2,3
+		int k=10;
+		arraySum(a, k);
 		 paixu(a);
 		 System.out.println(a.length/2);
 		paipai(a);
 	}
 
+	
+	
 	public static void paixu(int[] arr) {//希尔排序
 		for (int interval = arr.length / 2; interval > 0; interval = interval / 2) {
 			for (int i =interval; i < arr.length; i++) {
@@ -41,4 +45,32 @@ public class 排序 {
 		System.out.println(Arrays.toString(b));
 
 	}
+
+public static void arraySum(int arr[],int k)
+    {
+        if(arr==null||arr.length<3)
+            return;
+        for(int i=0;i<arr.length-2;i++)
+        {       
+
+            int left=i+1,right=arr.length-1;
+            while(left<right)
+            {
+                if(arr[i]+arr[left]+arr[right]==k)
+                {
+                    System.out.println(arr[i]+" "+arr[left]+" "+arr[right]);
+                    left++;
+                    right--;
+                }
+                else if(arr[i]+arr[left]+arr[right]<k)
+                {
+                    left++;
+                }
+                else {
+                    right--;
+                }
+                }
+            }
+
+  }
 }
